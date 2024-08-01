55a9f6b803cfbf58190270f7eb7baa97edf5d7fe
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2024-08-01 15:07:34 -0300

    delete folders

--- Clase 2/dist/index.672d4772.js
+++
@@ -1,6 +0,0 @@
-//DOM . DOCUMENT OBJECT MODEL
-document.getElementById("subscribeButton").addEventListener("click", function() {
-    alert("YA ES TARDE!");
-});
-
-//# sourceMappingURL=index.672d4772.js.map
